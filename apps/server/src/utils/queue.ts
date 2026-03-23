4331d5969b27d50fc25d738f966743668223a4d7
Nikhil Gupta <nikhil113016@gmail.com>
2026-03-23 13:48:52 +0530

    fixed queue

--- apps/server/src/utils/queue.ts
+++ apps/server/src/utils/queue.ts
@@ -1,8 +1,16 @@
 import { Queue } from "bullmq";
 
+import IORedis from "ioredis";
+
+const connection = new IORedis({
+  host: process.env.REDIS_HOST!,
+  port: Number(process.env.REDIS_PORT!),
+  password: process.env.REDIS_PASSWORD!,
+  tls: {},
+  maxRetriesPerRequest: null, 
+}) as any;
+
+
 export const assignmentQueue = new Queue("assignments", {
-  connection: {
-    host: "127.0.0.1",
-    port: 6379,
-  },
+  connection,
 });
